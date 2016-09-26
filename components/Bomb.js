d0622ee327d25b2b8b7ae327702af12d1d94ab30
daniellasheva <daniellasheva@gmail.com>
2016-09-26 00:09:38 -0400

    built initial state lab

---
+++ components/Bomb.js
@@ -0,0 +1,26 @@
+const React = require('react')
+
+class Bomb extends React.Component{
+  constructor(props){
+    super()
+    this.state={
+      secondsLeft: props.initialCount,
+    }
+  }
+
+  render(){
+    if (this.state.secondsLeft===0){
+    return(
+      <p>Boom!</p>
+    )
+  } else {
+    return (
+      <div>
+      {this.state.secondsLeft} seconds left before I go boom!
+      </div>
+    )
+  }
+  }
+}
+
+module.exports= Bomb;
